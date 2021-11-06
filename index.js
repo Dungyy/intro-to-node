a7eea17dd32e68d21454eda479b16dce9eee58e5
Erick Munoz <88725477+Dungyy@users.noreply.github.com>
2021-11-06 03:40:24 -0500

    Add files via upload
    into to node

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+var superhero = require('superhero-name-library');
+
+const emoji = require('random-food-emoji');
+
+var mySuperHeroName = superhero.random()
+
+console.log("Your Superhero name is: " + mySuperHeroName)
+console.log("Your Emoji is: " + emoji())
